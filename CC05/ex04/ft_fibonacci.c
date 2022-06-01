135f6a0e2185b2e40b24a66ad2973b8e036b4e4a
Joel Nottle <jnottle@F2N014.42adel.org.au>
2022-06-01 15:14:57 +0930

    modified:   .DS_Store 	new file:   ex04/.DS_Store 	modified:   ex04/ft_fibonacci.c 	modified:   ../.DS_Store

--- CC05/ex04/ft_fibonacci.c
+++ CC05/ex04/ft_fibonacci.c
@@ -6,7 +6,7 @@
 /*   By: jnottle <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 14:38:28 by jnottle           #+#    #+#             */
-/*   Updated: 2022/06/01 08:31:56 by jnottle          ###   ########.fr       */
+/*   Updated: 2022/06/01 13:27:24 by jnottle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,17 @@ int	ft_fibonacci(int nb)
 {
 	if (nb < 0)
 		return (-1);
-	if (nb <= 2)
-		return (nb);
-	return (ft_fibonacci(nb - 1) + ft_fibonacci(nb - 2));
+	if (nb == 0)
+		return (0);
+	if (nb == 1)
+		return (1);
+	else
+		return (ft_fibonacci(nb - 1) + ft_fibonacci(nb - 2));
 }
 /*
 int	main(void)
 {
 	printf("%d", ft_fibonacci(6));
-		return (0);
+	return (0);
 }
 */
